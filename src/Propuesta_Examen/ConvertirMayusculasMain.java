666fcde75b62840c362b1cc434097a333d69398e
Ruben De Haro Sánchez <rubensanchezdh@gmail.com>
2023-10-31 08:24:25 +0100

    Propuesta_Examen

--- src/Propuesta_Examen/ConvertirMayusculasMain.java
+++ src/Propuesta_Examen/ConvertirMayusculasMain.java
@@ -9,9 +9,9 @@ public class ConvertirMayusculasMain {
     static final String jar = "convertir.jar";
     static final String clase = "ConvertirMayusculas.class";
     public static void main(String[] args) throws IOException {
-        InputStreamReader inS = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(inS);
-        String linea = br.readLine();
+
+        System.out.println("Introducir texto: ");
+        String linea = new Scanner(System.in).nextLine();
 
         String[] comandoCompletoJar = {comando, opcion, jar, clase};
         ProcessBuilder pB = new ProcessBuilder(comandoCompletoJar);
@@ -19,7 +19,23 @@ public class ConvertirMayusculasMain {
 
         try{
             Process p = pB.start();
-        }catch (IOException e){
+            OutputStream os = p.getOutputStream();
+            OutputStreamWriter osW = new OutputStreamWriter(os);
+            PrintWriter pW = new PrintWriter(osW);
+            pW.println(linea);
+            pW.close();
+
+            int codRetorno = p.waitFor();
+            System.out.println((codRetorno == 0 ? "Ejecución del subproceso realizada con exito" : "Ejecución del subproceso erronea"));
+
+            if(codRetorno == 0){
+                InputStream is = p.getInputStream();
+                InputStreamReader isR = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isR);
+            }else{
+                System.err.println("Saliendo del programa sin exito");
+            }
+        }catch (IOException | InterruptedException e){
             System.err.println("Error detectado: " + e.getMessage());
         }
     }
