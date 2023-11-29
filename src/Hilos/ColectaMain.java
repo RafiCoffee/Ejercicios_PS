4a62f21575fd2fa6d47149be9781eea446a7268d
Ruben <rubensanchezdh@hotmail.com>
2023-11-29 15:22:25 +0100

    Ejercicios Colecta Hecho

--- src/Hilos/ColectaMain.java
+++ src/Hilos/ColectaMain.java
@@ -3,8 +3,24 @@ package Hilos;
 import java.util.Random;
 
 public class ColectaMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Colecta colecta = new Colecta();
+        Thread Ruben = new Thread(new Voluntario("Ruben", colecta));
+        Thread Javi = new Thread(new Voluntario("Javi", colecta));
+        Thread Paula = new Thread(new Voluntario("Paula", colecta));
+        Thread Anabel = new Thread(new Voluntario("Anabel", colecta));
 
+        Ruben.start();
+        Javi.start();
+        Paula.start();
+        Anabel.start();
+
+        Ruben.join();
+        Javi.join();
+        Paula.join();
+        Anabel.join();
+
+        System.out.println("\nEl objetivo de colecta ha sido alcanzado con " + colecta.getColectaActual() + "\n");
     }
 }
 
@@ -46,21 +62,23 @@ class Voluntario implements Runnable{
     public void run(){
         System.out.println("Soy el voluntario " + this.nombre + " y voy a comenzar a recolectar");
 
-        while (!colecta.colectaTerminada()){
-            long tiempoColecta = new Random().nextLong(10, 200);
-            int recolecta = new Random().nextInt(4, 25);
-            this.totalColectaIndividual += recolecta;
+        try{
+            while (!colecta.colectaTerminada()){
+                long tiempoColecta = new Random().nextLong(10, 200);
+                int recolecta = new Random().nextInt(4, 25);
+                this.totalColectaIndividual += recolecta;
 
-            try {
                 Thread.sleep(tiempoColecta);
 
                 System.out.println(this.nombre + " ha recolectado " + recolecta);
                 colecta.incrementarColectaActual(recolecta);
                 numeroRecolectas++;
 
-            } catch (InterruptedException e) {
-                System.err.println("Error detectado: " + e.getMessage());
             }
+
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.err.println("Error detectado: " + e.getMessage());
         }
 
         System.out.println("Soy " + this.nombre + " y he recolectado " + this.numeroRecolectas + " veces con un total de " + this.totalColectaIndividual);
